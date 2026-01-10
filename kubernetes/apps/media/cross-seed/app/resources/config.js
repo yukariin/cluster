f48c2908833168fc82f31e71ac3bb9686370bfd2
Yukari Yakumo <mistelinn@gmail.com>
2026-01-11 00:22:40 +0300

    Update cross-seed config

--- kubernetes/apps/media/cross-seed/app/resources/config.js
+++ kubernetes/apps/media/cross-seed/app/resources/config.js
@@ -14,22 +14,32 @@ function fetchIndexers(baseUrl, apiKey, tag) {
 }
 
 export default {
+  // Basic
   action: "inject",
   apiKey: process.env.CROSS_SEED_API_KEY,
+  includeNonVideos: true,
+  rssCadence: "20min",
+  skipRecheck: true,
+  // Blocklist
   blockList: ["category:manual"],
+  // Container
+  outputDir: null,
+  port: Number(process.env.CROSS_SEED_PORT),
+  // Partial Matching
+  matchMode: "partial",
   linkCategory: "cross-seed",
   linkDirs: ["/media/torrents/complete/cross-seed"],
   linkType: "hardlink",
-  matchMode: "partial",
-  outputDir: null,
-  port: Number(process.env.CROSS_SEED_PORT),
-  skipRecheck: true,
+  // Searching
   excludeRecentSearch: "3 days",
   excludeOlder: "2 weeks",
   searchCadence: "1 day",
-  torrentClients: ["qbittorrent:http://qbittorrent.media.svc.cluster.local"],
+  // ARR searching
   radarr: [`http://radarr.media.svc.cluster.local/?apikey=$${process.env.RADARR__AUTH__APIKEY}`,],
   sonarr: [`http://sonarr.media.svc.cluster.local/?apikey=$${process.env.SONARR__AUTH__APIKEY}`,],
+  // Prowlarr
   torznab: fetchIndexers("http://prowlarr.media.svc.cluster.local", process.env.PROWLARR__AUTH__APIKEY, "cross-seed"),
+  // Torrent Clients
+  torrentClients: ["qbittorrent:http://qbittorrent.media.svc.cluster.local"],
   useClientTorrents: true,
 };
